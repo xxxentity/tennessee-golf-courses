6f45e44fae8bb9e031b17f06a829392d3c372ec5
entity <entity.@entitys-MacBook-Air.local>
2025-08-29 09:43:32 -0500

    Test PHP processing in news article

--- news/tour-championship-2025-atlanta-complete-tournament-recap-fleetwood-first-win.php
+++ news/tour-championship-2025-atlanta-complete-tournament-recap-fleetwood-first-win.php
@@ -1,3 +1,22 @@
+<?php
+require_once '../includes/init.php';
+require_once '../includes/profile-helpers.php';
+require_once '../includes/seo.php';
+
+$article_data = [
+    'title' => 'Tour Championship 2025: Complete Atlanta Tournament Recap and Tommy Fleetwood\'s Historic First Win',
+    'description' => 'Complete recap of the 2025 Tour Championship.',
+    'image' => '/images/news/tour-championship-2025-atlanta-complete-tournament-recap-fleetwood-first-win/main.webp',
+    'type' => 'article',
+    'author' => 'Cole Harrington',
+    'date' => '2025-08-25',
+    'category' => 'Tournament News'
+];
+
+SEO::setupArticlePage($article_data);
+$article_slug = 'tour-championship-2025-atlanta-complete-tournament-recap-fleetwood-first-win';
+$article_title = 'Tour Championship 2025: Complete Atlanta Tournament Recap and Tommy Fleetwood\'s Historic First Win';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,7 +25,8 @@
     <title>Tour Championship 2025</title>
 </head>
 <body>
-    <h1>WORKING TEST</h1>
-    <p>If you can see this, the file is working.</p>
+    <h1>PHP WORKING TEST</h1>
+    <p>If you can see this, PHP processing works.</p>
+    <p>Article title: <?php echo htmlspecialchars($article_title); ?></p>
 </body>
 </html>
